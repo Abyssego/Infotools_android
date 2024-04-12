bc5d88b5a70d887ebd64a313fb2ca3713a493912
Abyssego <112755826+Abyssego@users.noreply.github.com>
2024-04-12 10:37:15 +0200

    Add files via upload

---
+++ infotoolsFinal/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace infotoolsFinal
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
